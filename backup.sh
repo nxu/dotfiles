bd15e24d0a5c9663ac54873be51b11c355512249
Zsolt Fekete <nxu@nxu.hu>
2026-02-28 09:16:20 +0100

    Update backup script

--- backup.sh
+++ backup.sh
@@ -14,8 +14,10 @@ fi
 cd $WORKDIR || return 1
 mkdir $BACKUP_ID
 
-# Penz.nxu.hu
-tar -zcvf "${BACKUP_ID}/penz.tar.gz" "/var/www/penz/shared/database/" >/dev/null 2>&1
+# Receptek
+podman compose -f /root/compose.yaml stop mealiecp
+cp -r /root/docker-volumes/mealie "${BACKUP_ID}/mealie"
+podman compose -f /root/compose.yaml start mealie
 
 # Caddyfile
 cp /etc/caddy/Caddyfile "${BACKUP_ID}/Caddyfile"
@@ -26,4 +28,12 @@ cp "/root/backup.sh" "${BACKUP_ID}/backup.sh"
 # Compress everything
 tar -zcvf "${BACKUP_ID}.tar.gz" $BACKUP_ID >/dev/null 2>&1
 
+# Delete old backups
+for old_backup in $(ls /root/backup/|grep "^[0-9\-]*$"|sort -nr|sed "1,3 d")
+do
+	echo "Deleting folder: $old_backup"
+  rm -rf "/root/backup/$old_backup" || true
 
+  echo "Deleting archive: $old_backup.tar.gz"
+  rm -rf "/root/backup/$old_backup.tar.gz" || true
+done
