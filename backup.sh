cddfb73d09e8d1bbcfb7a3f134a8bb50e97abf92
Zsolt Fekete <nxu@nxu.hu>
2026-03-01 13:54:37 +0100

    Update backup script

--- backup.sh
+++ backup.sh
@@ -15,18 +15,29 @@ cd $WORKDIR || return 1
 mkdir $BACKUP_ID
 
 # Receptek
-podman compose -f /root/compose.yaml stop mealiecp
-cp -r /root/docker-volumes/mealie "${BACKUP_ID}/mealie"
-podman compose -f /root/compose.yaml start mealie
+docker compose -f /root/compose.yaml stop web_recipes
+docker compose -f /root/compose.yaml stop db_recipes
+cp -r /root/docker-volumes/tandoor "${BACKUP_ID}/tandoor"
+docker compose -f /root/compose.yaml start db_recipes
+docker compose -f /root/compose.yaml start web_recipes
 
 # Caddyfile
 cp /etc/caddy/Caddyfile "${BACKUP_ID}/Caddyfile"
+
+# Local fail2ban config
+mkdir "${BACKUP_ID}/fail2ban"
+cd /etc/fail2ban
+find . -name '*.local' | cpio -pdm "$WORKDIR/${BACKUP_ID}/fail2ban"
+cd $WORKDIR || return 1
 
 # Backup script itself
 cp "/root/backup.sh" "${BACKUP_ID}/backup.sh"
 
 # Compress everything
 tar -zcvf "${BACKUP_ID}.tar.gz" $BACKUP_ID >/dev/null 2>&1
+
+# Delete source folder
+rm -rf $BACKUP_ID
 
 # Delete old backups
 for old_backup in $(ls /root/backup/|grep "^[0-9\-]*$"|sort -nr|sed "1,3 d")
